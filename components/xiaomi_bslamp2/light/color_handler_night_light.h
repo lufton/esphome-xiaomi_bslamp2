0dc7b2dd03139bc7503d1f89e4fe64f3ba9bbfdc
Дубовик Максим <lufton@gmail.com>
2023-10-09 00:51:49 +0300

    Update color_handler_night_light.h

--- components/xiaomi_bslamp2/light/color_handler_night_light.h
+++ components/xiaomi_bslamp2/light/color_handler_night_light.h
@@ -37,19 +37,19 @@ class ColorHandlerNightLight : public ColorHandler {
     // Based on measurements using the original device firmware, so it
     // matches the night light of the original firmware.
     if (v.get_color_mode() == light::ColorMode::COLOR_TEMPERATURE) {
-      red = 0.968f;
-      green = 0.968f;
-      blue = 0.972f;
+      red = 0.960f;
+      green = 0.960f;
+      blue = 0.960f;
       white = 0.0f;
     }
     // In RGB mode, the selected color is used to give the night light a
     // specific color, instead of the default. This is a nice extra for
     // this firmware, as the original firmware does not support it.
     else {
-      red = esphome::lerp(v.get_red(), 0.9997f, 0.9680f);
-      green = esphome::lerp(v.get_green(), 0.9997f, 0.9680f);
+      red = esphome::lerp(v.get_red(), 0.9997f, 0.960f);
+      green = esphome::lerp(v.get_green(), 0.9997f, 0.960f);
       auto blue_scale = (v.get_red() + v.get_green()) / 2.0f;
-      auto blue_max = esphome::lerp(blue_scale, 0.9640f, 0.9720f);
+      auto blue_max = esphome::lerp(blue_scale, 0.9640f, 0.960f);
       blue = esphome::lerp(v.get_blue(), 0.9997f, blue_max);
       white = 0.0f;
     }
